45c74d40d0a14f9259f5ac096a509753b922a2d8
Witold Serwatka <witold.serwatka@pm.me>
2020-01-06 09:47:46 +0100

    added board functions

--- src/main/game/Board.java
+++ src/main/game/Board.java
@@ -7,4 +7,33 @@ public class Board {
     private Apple apple;
     private Snake snake;
 
+    Board(int width, int height){
+        this.lowerLeft = new Vector2D(0,0);
+        this.upperRight = new Vector2D(width-1,height-1);
+        this.apple = new Apple(new Vector2D((3/4)*width,(1/2)*height));
+        //this.snake
+    }
+
+    private void generateApple(){
+        Vector2D applePosition;
+
+        do{
+            applePosition = new Vector2D(
+                    (int)(Math.random()*this.upperRight.getX()),
+                    (int)(Math.random()*this.upperRight.getY())
+            );
+        }while(!(
+                applePosition.follows(lowerLeft) &&
+                applePosition.precedes(upperRight) &&
+                !this.snake.getBody().contains(applePosition)) &&
+                !this.snake.getHead().equals(applePosition)
+        );
+
+        this.apple = new Apple(applePosition);
+    }
+
+    private boolean validateSnakePosition(){
+        return this.snake.getHead().follows(lowerLeft) && this.snake.getHead().precedes(upperRight);
+    }
+
 }
--- src/main/game/Snake.java
+++ src/main/game/Snake.java
@@ -1,8 +1,34 @@
 package main.game;
 
+import java.util.HashSet;
+
 public class Snake {
 
     private Vector2D head;
-    private
+    private HashSet<Vector2D> body;
+    private MapOrientation orientation;
+
+    Snake(Vector2D position,int length){
+        this.head = position;
+        this.orientation = MapOrientation.RIGHT;
+
+        for(int i=1;i<length;i++){
+            body.add(new Vector2D(position.getX()-i,position.getY()));
+        }
+    }
+
+    public HashSet<Vector2D> getBody() {
+        return body;
+    }
+
+    public MapOrientation getOrientation() {
+        return orientation;
+    }
+
+    public Vector2D getHead() {
+        return head;
+    }
+
+    
 
 }
